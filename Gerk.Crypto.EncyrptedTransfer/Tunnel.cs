d34e18f7e8210b803dcc7217bd05529b2d2277e7
Benji Altman <benji@altmansoftwaredesign.com>
2021-11-08 22:26:54 -0600

    Documentation

--- Gerk.Crypto.EncyrptedTransfer/Tunnel.cs
+++ Gerk.Crypto.EncyrptedTransfer/Tunnel.cs
@@ -13,27 +13,6 @@ using Gerk.BinaryExtension;
 namespace Gerk.Crypto.EncyrptedTransfer
 {
 	/// <summary>
-	/// Error code for errors that happen during the tunnel creation
-	/// </summary>
-	/// <seealso cref="Tunnel.CreateInitiator(Stream, IEnumerable{RSAParameters}, RSACryptoServiceProvider, out TunnelCreationError, bool)"/>
-	/// <seealso cref="Tunnel.CreateResponder(Stream, IEnumerable{RSAParameters}, RSACryptoServiceProvider, out TunnelCreationError, bool)"/>
-	public enum TunnelCreationError
-	{
-		/// <summary>
-		/// All good, no errors.
-		/// </summary>
-		NoError = 0,
-		/// <summary>
-		/// The other side of the tunnel provided an invalid public key.
-		/// </summary>
-		RemoteDoesNotHaveValidPublicKey,
-		/// <summary>
-		/// The other side of the tunnel failed to prove their identiity.
-		/// </summary>
-		RemoteFailedToVierfyItself,
-	}
-
-	/// <summary>
 	/// A <see cref="Stream"/> for end to end encrypted and secure transfering of data. Data is only written to the underlying stream in blocks. You can complete a block that needs to be written by using <see cref="FlushWriter"/>. When reading, wherever you expect a block to be completed by a <see cref="FlushWriter"/> you should call <see cref="FlushReader"/> to jump to the end of the block. Wrapping this stream with a <see cref="StreamReader"/> or <see cref="StreamWriter"/> is not currently supported. Rather you are encouraged to use a <see cref="BinaryReader"/> and <see cref="BinaryWriter"/>.
 	/// </summary>
 	public class Tunnel : Stream
@@ -52,14 +31,25 @@ namespace Gerk.Crypto.EncyrptedTransfer
 		private CryptoStream readStream;
 		private CryptoStream writeStream;
 		private readonly Stream underlyingStream;
+
+		/// <summary>
+		/// Total number of bytes read so far. Really only maters that it is correct mod <see cref="BlockSize"/>.
+		/// </summary>
 		private ulong bytesRead = 0;
+		/// <summary>
+		/// Total number of bytes written so far. Really only maters that it is correct mod <see cref="BlockSize"/>.
+		/// </summary>
 		private ulong bytesWritten = 0;
+
 		/// <summary>
 		/// The size in bytes of each block. 
 		/// </summary>
 		public static uint BlockSize => AES_BLOCK_SIZE;
 
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+		/// <summary>
+		/// Should <see cref="underlyingStream"/> be disposed by this stream.
+		/// </summary>
 		private readonly bool leaveOpen;
 #endif
 		/// <summary>
@@ -68,23 +58,25 @@ namespace Gerk.Crypto.EncyrptedTransfer
 		public RSAParameters remotePublicKey { private set; get; }
 
 		/// <summary>
-		/// 
+		/// Constructor
 		/// </summary>
-		/// <param name="stream"></param>
-		/// <param name="leaveOpen"></param>
+		/// <param name="stream">The underlying stream.</param>
+		/// <param name="leaveOpen">Whether to leave open or not.</param>
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
 		private Tunnel(Stream stream, bool leaveOpen = false)
 		{
-			this.underlyingStream = stream;
 			this.leaveOpen = leaveOpen;
-		}
 #else
 		private Tunnel(Stream stream)
 		{
+#endif
 			this.underlyingStream = stream;
 		}
-#endif
 
+		/// <summary>
+		/// Creates <see cref="CryptoStream"/>s.
+		/// </summary>
+		/// <param name="sharedKey">The initialized AES key.</param>
 		private void InitCryptoStreams(Aes sharedKey)
 		{
 			var dec = sharedKey.CreateDecryptor();
@@ -98,6 +90,12 @@ namespace Gerk.Crypto.EncyrptedTransfer
 #endif
 		}
 
+		/// <summary>
+		/// Reads an AES key from <paramref name="bw"/> that was encrypted with <paramref name="rsa"/>.
+		/// </summary>
+		/// <param name="bw"></param>
+		/// <param name="rsa"></param>
+		/// <returns>The AES key</returns>
 		private static Aes ReadAesKey(BinaryReader bw, RSACryptoServiceProvider rsa)
 		{
 			var aes = aeskey();
@@ -116,6 +114,12 @@ namespace Gerk.Crypto.EncyrptedTransfer
 			return aes;
 		}
 
+		/// <summary>
+		/// Encrypts the AES key <paramref name="aes"/> with RSA key <paramref name="rsa"/> and write to binary writer <paramref name="bw"/>.
+		/// </summary>
+		/// <param name="aes"></param>
+		/// <param name="bw"></param>
+		/// <param name="rsa"></param>
 		private static void WriteAesKey(Aes aes, BinaryWriter bw, RSACryptoServiceProvider rsa)
 		{
 			using (var memStream = new MemoryStream())
@@ -126,6 +130,10 @@ namespace Gerk.Crypto.EncyrptedTransfer
 			}
 		}
 
+		/// <summary>
+		/// Sets up an AES key without initializing it.
+		/// </summary>
+		/// <returns></returns>
 		private static Aes aeskey()
 		{
 			var output = Aes.Create();
@@ -134,6 +142,15 @@ namespace Gerk.Crypto.EncyrptedTransfer
 			return output;
 		}
 
+		/// <summary>
+		/// Initiates handshake to setup secure connection over <see cref="Stream"/>.
+		/// </summary>
+		/// <param name="stream">The underlying stream. Usually expected to be a network stream.</param>
+		/// <param name="remotePublicKeys">The public keys that are allowed. Can be left <see langword="null"/> to allow connection to anyone. Key can also be found later using <see cref="remotePublicKey"/> property.</param>
+		/// <param name="localPrivateKey">The private key you use to connect.</param>
+		/// <param name="error">An error message for if something goes wrong.</param>
+		/// <param name="leaveOpen">True to not close the underlying stream when the <see cref="Tunnel"/> is closed.</param>
+		/// <returns>The new stream that wraps <paramref name="stream"/> with end to end encyption.</returns>
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
 		public static Tunnel CreateInitiator(Stream stream, IEnumerable<RSAParameters> remotePublicKeys, RSACryptoServiceProvider localPrivateKey, out TunnelCreationError error, bool leaveOpen = false)
 #else
@@ -141,9 +158,9 @@ namespace Gerk.Crypto.EncyrptedTransfer
 #endif
 		{
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
-			Tunnel output = new Tunnel(stream, leaveOpen);
+			var output = new Tunnel(stream, leaveOpen);
 #else
-			Tunnel output = new Tunnel(stream);
+			var output = new Tunnel(stream);
 #endif
 			try
 			{
@@ -168,7 +185,7 @@ namespace Gerk.Crypto.EncyrptedTransfer
 						// read remote public key
 						remotePublicKey.ImportCspBlob(reader.ReadBinaryData());
 						output.remotePublicKey = remotePublicKey.ExportParameters(false);
-						if (!remotePublicKeys.Any(x => x.Modulus.SequenceEqual(output.remotePublicKey.Modulus)))
+						if (remotePublicKeys != null && !remotePublicKeys.Any(x => x.Modulus.SequenceEqual(output.remotePublicKey.Modulus)))
 						{
 							output.Dispose();
 							error = TunnelCreationError.RemoteDoesNotHaveValidPublicKey;
@@ -197,7 +214,15 @@ namespace Gerk.Crypto.EncyrptedTransfer
 			}
 		}
 
-
+		/// <summary>
+		/// Responds to a handshake to setup secure connection over <see cref="Stream"/>.
+		/// </summary>
+		/// <param name="stream">The underlying stream. Usually expected to be a network stream.</param>
+		/// <param name="remotePublicKeys">The public keys that are allowed. Can be left <see langword="null"/> to allow connection from anyone. Key can also be found later using <see cref="remotePublicKey"/> property.</param>
+		/// <param name="localPrivateKey">The private key you use to connect.</param>
+		/// <param name="error">An error message for if something goes wrong.</param>
+		/// <param name="leaveOpen">True to not close the underlying stream when the <see cref="Tunnel"/> is closed.</param>
+		/// <returns>The new stream that wraps <paramref name="stream"/> with end to end encyption.</returns>
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
 		public static Tunnel CreateResponder(Stream stream, IEnumerable<RSAParameters> remotePublicKeys, RSACryptoServiceProvider localPrivateKey, out TunnelCreationError error, bool leaveOpen = false)
 #else
@@ -205,9 +230,9 @@ namespace Gerk.Crypto.EncyrptedTransfer
 #endif
 		{
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
-			Tunnel output = new Tunnel(stream, leaveOpen);
+			var output = new Tunnel(stream, leaveOpen);
 #else
-			Tunnel output = new Tunnel(stream);
+			var output = new Tunnel(stream);
 #endif
 			try
 			{
@@ -221,7 +246,7 @@ namespace Gerk.Crypto.EncyrptedTransfer
 					{
 						remotePublicKey.ImportCspBlob(reader.ReadBinaryData());
 						output.remotePublicKey = remotePublicKey.ExportParameters(false);
-						if (!remotePublicKeys.Any(x => x.Modulus.SequenceEqual(output.remotePublicKey.Modulus)))
+						if (remotePublicKeys != null && !remotePublicKeys.Any(x => x.Modulus.SequenceEqual(output.remotePublicKey.Modulus)))
 						{
 							output.Dispose();
 							error = TunnelCreationError.RemoteDoesNotHaveValidPublicKey;
@@ -261,18 +286,28 @@ namespace Gerk.Crypto.EncyrptedTransfer
 			}
 		}
 
+		/// <inheritdoc/>
 		public override bool CanRead => true;
-
+		/// <inheritdoc/>
 		public override bool CanSeek => false;
-
+		/// <inheritdoc/>
 		public override bool CanWrite => true;
-
+		/// <summary>
+		/// Not supported
+		/// </summary>
 		public override long Length => throw new NotSupportedException();
-
+		/// <summary>
+		/// Not supported
+		/// </summary>
 		public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
-
+		/// <summary>
+		/// Flushes the underlying stream. Does not call <see cref="FlushReader"/> or <see cref="FlushWriter"/>.
+		/// </summary>
 		public override void Flush() => underlyingStream.Flush();
 
+		/// <summary>
+		/// Writes 0s to the stream until the end the current block so it can be sent.
+		/// </summary>
 		public virtual void FlushWriter()
 		{
 			int bytesToWrite = (int)(BlockSize - (bytesWritten % BlockSize));
@@ -280,6 +315,9 @@ namespace Gerk.Crypto.EncyrptedTransfer
 				Write(new byte[bytesToWrite], 0, bytesToWrite);
 		}
 
+		/// <summary>
+		/// Skips to the end of the current block. Meant to skip the 0s wrote by <see cref="FlushWriter"/>.
+		/// </summary>
 		public virtual void FlushReader()
 		{
 			int bytesToRead = (int)(BlockSize - bytesRead % BlockSize);
@@ -287,17 +325,18 @@ namespace Gerk.Crypto.EncyrptedTransfer
 				Write(new byte[bytesToRead], 0, bytesToRead);
 		}
 
+		/// <inheritdoc/>
 		public override int Read(byte[] buffer, int offset, int count)
 		{
 			var read = readStream.Read(buffer, offset, count);
 			bytesRead += (ulong)read;
 			return read;
 		}
-
+		/// <inheritdoc/>
 		public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
-
+		/// <inheritdoc/>
 		public override void SetLength(long value) => throw new NotSupportedException();
-
+		/// <inheritdoc/>
 		public override void Write(byte[] buffer, int offset, int count)
 		{
 			bytesWritten += (ulong)count;
@@ -305,9 +344,9 @@ namespace Gerk.Crypto.EncyrptedTransfer
 		}
 
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+		/// <inheritdoc/>
 		public override async ValueTask DisposeAsync()
 		{
-			sharedKey.Dispose();
 			var a = writeStream.DisposeAsync();
 			var b = readStream.DisposeAsync();
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
@@ -319,10 +358,9 @@ namespace Gerk.Crypto.EncyrptedTransfer
 			await b;
 		}
 #endif
-
+		/// <inheritdoc/>
 		public override void Close()
 		{
-			sharedKey.Dispose();
 			writeStream?.Close();
 			readStream?.Close();
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
---
+++ Gerk.Crypto.EncyrptedTransfer/TunnelCreationError.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Gerk.Crypto.EncyrptedTransfer
+{
+	/// <summary>
+	/// Error code for errors that happen during the tunnel creation
+	/// </summary>
+	/// <seealso cref="Tunnel.CreateInitiator(Stream, IEnumerable{RSAParameters}, RSACryptoServiceProvider, out TunnelCreationError, bool)"/>
+	/// <seealso cref="Tunnel.CreateResponder(Stream, IEnumerable{RSAParameters}, RSACryptoServiceProvider, out TunnelCreationError, bool)"/>
+	public enum TunnelCreationError
+	{
+		/// <summary>
+		/// All good, no errors.
+		/// </summary>
+		NoError = 0,
+		/// <summary>
+		/// The other side of the tunnel provided an invalid public key.
+		/// </summary>
+		RemoteDoesNotHaveValidPublicKey,
+		/// <summary>
+		/// The other side of the tunnel failed to prove their identiity.
+		/// </summary>
+		RemoteFailedToVierfyItself,
+	}
+}
